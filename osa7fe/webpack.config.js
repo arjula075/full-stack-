6b3e18f2fc5b3f202c09c931794fa4a13e577f9e
Ari Lahti <ari.iska.lahti@gmail.com>
2018-12-06 12:23:02 +0200

    Tehtävä 7.15

---
+++ osa7fe/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/react'],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config
